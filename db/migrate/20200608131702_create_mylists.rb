30fe67cb8dfb2ac6dc79048907ed89d7d096dda6
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-08 22:20:32 +0900

    マイグレーションファイルの記述

--- db/migrate/20200608131702_create_mylists.rb
+++ db/migrate/20200608131702_create_mylists.rb
@@ -1,7 +1,8 @@
 class CreateMylists < ActiveRecord::Migration[5.2]
   def change
     create_table :mylists do |t|
-
+      t.references :user, null:false
+      t.references :video, null:false
       t.timestamps
     end
   end
