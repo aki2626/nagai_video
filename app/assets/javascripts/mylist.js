542332661a4dafc7fcd7a063f0e5cf36a55e23dc
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-09 16:59:51 +0900

    mylistjsの編集
    クリックによってright_main_contains部分が切り替わるように編集

--- app/assets/javascripts/mylist.js
+++ app/assets/javascripts/mylist.js
@@ -1,8 +1,29 @@
 $(function(){
   $('.left_box__icon').on("click", function(e){
     e.preventDefault();
+    $('.relation_movies').removeClass("display_show");
     $('.relation_movies').addClass("display_none");
     $('.mylists_add').removeClass("display_none");
     $('.mylists_add').addClass("display_show");
+  });
+  $('.mylists_add__header--cancel').on("click", function(e){
+    e.preventDefault();
+    $('.mylists_add').removeClass("display_show");
+    $('.mylists_add').addClass("display_none");
+    $('.relation_movies').removeClass("display_none");
+    $('.relation_movies').addClass("display_show");
+  });
+  $('.user_mylists__mylist--new_mylist').on("click", function(e){
+    e.preventDefault();
+    $('.user_mylists').addClass("display_none");
+    $('.create_mylist').removeClass("display_none");
+    $('.create_mylist').addClass("display_show");
+  });
+  $('.mylist_btn__cancel').on("click", function(e){
+    e.preventDefault();
+    $('.create_mylist').removeClass("display_show");
+    $('.create_mylist').addClass("display_none");
+    $('.user_mylists').removeClass("display_none");
+    $('.user_mylists').addClass("display_show");
   });
 });
