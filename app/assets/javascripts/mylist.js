d4dcff84fc0109c64d5cd8b6d4b62a07bd1424a2
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-09 16:08:26 +0900

    mylist.jsの編集
    クリック時にmylists_addが表示されるようにjsの作成

--- app/assets/javascripts/mylist.js
+++ app/assets/javascripts/mylist.js
@@ -1,6 +1,8 @@
 $(function(){
   $('.left_box__icon').on("click", function(e){
     e.preventDefault();
-    
-  })
+    $('.relation_movies').addClass("display_none");
+    $('.mylists_add').removeClass("display_none");
+    $('.mylists_add').addClass("display_show");
+  });
 });
