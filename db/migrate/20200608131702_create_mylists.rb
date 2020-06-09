4222b0cac9be96ffcd95479710a2ff5bb06c11f9
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-09 13:46:32 +0900

    mylistsテーブルにexplainカラムの追加

--- db/migrate/20200608131702_create_mylists.rb
+++ db/migrate/20200608131702_create_mylists.rb
@@ -2,7 +2,8 @@ class CreateMylists < ActiveRecord::Migration[5.2]
   def change
     create_table :mylists do |t|
       t.references :user, null:false, foreign_key: true
-      t.string :name, null: false
+      t.string :name,     null: false
+      t.string :exolain,  null: false
       t.timestamps
     end
   end
