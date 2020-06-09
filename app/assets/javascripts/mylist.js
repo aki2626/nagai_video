3ba2b5a5a7e329512a4a077df823352bfb676e3f
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-09 16:02:59 +0900

    mylist部分のhamlの作成

--- app/assets/javascripts/mylist.js
+++ app/assets/javascripts/mylist.js
@@ -1,12 +1,6 @@
 $(function(){
-  $('#hover_menu').hover(
-    function(e){
-      $('.menu').removeClass("display_none");
-      $('.menu').addClass("display_show");
-    },
-    function(e) {
-      $('.menu').removeClass("display_show");
-      $('.menu').addClass("display_none");
-    }
-  );
+  $('.left_box__icon').on("click", function(e){
+    e.preventDefault();
+    
+  })
 });
