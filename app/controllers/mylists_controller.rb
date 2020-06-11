1542373c36dc337d7b68eedc8bce3df14815606f
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-11 11:13:45 +0900

    mylist#addの記述
    railsにおいて行えるようになった。

--- app/controllers/mylists_controller.rb
+++ app/controllers/mylists_controller.rb
@@ -1,7 +1,8 @@
 class MylistsController < ApplicationController
-  before_action :set_video
+  # before_action :set_video
 
   def create
+    @video = Video.find(params[:video_id])
     @mylist = Mylist.new(mylist_params)
     @mylist.videos << @video
     binding.pry
@@ -12,10 +13,23 @@ class MylistsController < ApplicationController
     end
   end
 
-  private
-  def set_video
-    @video = Video.find(params[:video_id])
+  def add
+    @video = Video.find(params[:id])
+    @mylist = Mylist.find(params[:video_id])
+    # ここのparamsのidは中間テーブルの関係でidとvideoが入れ替わるため。
+    @mylist.videos << @video
+    if @mylist.save!
+      redirect_to root_path, notice: '動画をマイリストに追加しました'
+    else
+      redirect_to  root_path
+    end
   end
+
+  private
+  # def set_video
+  #   binding.pry
+  #   @video = Video.find(params[:video_id])
+  # end
   def mylist_params
     params.require(:mylist).permit(:name,:explain, video_ids: []).merge(user_id: current_user.id)
   end
--- app/models/mylist.rb
+++ app/models/mylist.rb
@@ -2,5 +2,5 @@ class Mylist < ApplicationRecord
   belongs_to :user
   has_many   :video_mylists
   has_many   :videos, through: :video_mylists
-  validates  :name, presence: true, uniqueness: true
+  validates  :name, presence: true
 end
