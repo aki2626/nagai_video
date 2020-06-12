eaa3a35603e242902740f4affea9739e693762f9
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-12 15:36:06 +0900

    view_historyテーブルのマイグレーションファイルの作成

--- db/migrate/20200612063245_create_viewing_histories.rb
+++ db/migrate/20200612063245_create_viewing_histories.rb
@@ -1,7 +1,8 @@
 class CreateViewingHistories < ActiveRecord::Migration[5.2]
   def change
     create_table :viewing_histories do |t|
-
+      t.references :user, foreign_key: true
+      t.references :video,  foreign_key: true
       t.timestamps
     end
   end
