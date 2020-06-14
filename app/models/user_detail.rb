e0e7020af7910d28940c67d3b514095f94f4d60a
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-14 14:05:29 +0900

    image_uploaderの導入

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -2,6 +2,8 @@ class UserDetail < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
+  mount_uploader :image, ImageUploader
+  
   belongs_to :user, optional: true
   validates :gender, :prefecture_id, :birth_date, presence: true
 end
