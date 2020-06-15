73079841b04a26d0d62530fb652e0a34b791b793
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-15 15:13:13 +0900

    user#update関連の記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,31 +11,31 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    unless @user.valid?
+    if  @user.update_attributes(user_params)
+    else 
       flash.now[:alert] = @user.errors.full_messages
       render :edit and return
     end
-    session["devise.regist_data"] = {user: @user.attributes}
-    session["devise.regist_data"][:user]["password"] = params[:user][:password]
-    @user_detail = @user.build_user_detail
+    @user_detail = UserDetail.find(params[:id])
     render :edit_user_detail
   end
 
   def update_user_detail
-    @user = User.find(session["devise.regist_data"]["user"])
-    @user_detail = UserDetail.find(user_detail_params)
-    unless @user_detail.valid?
+    @user_detail = UserDetail.find(params[:id])
+    if @user_detail.update_attributes(user_detail_params)
+    else 
       flash.now[:alert] = @user_detail.errors.full_messages
       render :new_user_detail and return
     end
-    @user.build_user_detail(@user_detail.attributes)
-    @user.update
-    session["devise.regist_data"]["user"].clear
+    @user = User.find(params[:id])
     sign_in(:user, @user)
   end
 
   protected
 
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+  end
   def user_detail_params
     params.require(:user_detail).permit(:gender, :prefecture_id, :birth_date, :image)
   end
