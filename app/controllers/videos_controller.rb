a7ce4cc9152960047788c76f948d98c15cf7c1ad
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-16 16:28:40 +0900

    videos#showでのインスタンス変数の定義
    @commentで新規投稿のため、@commentsで一覧表示のためのインスタンス変数

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -30,6 +30,8 @@ class VideosController < ApplicationController
   def show
     @video = Video.find(params[:id])
     @mylist = Mylist.new
+    @comment = Comment.new
+    @comments = @video.comments.includes(:user)
 
     @user = @video.user
     if user_signed_in?
