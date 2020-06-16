a6eb4b28aa8f22e1c1d0d3e3d65ea4fdb525e5a8
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-16 16:55:40 +0900

    comments#createの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,19 @@
 class CommentsController < ApplicationController
   def create
+    @video = Video.find(params[:video_id])
+    @comment = @video.comments.new(comment_params)
+    if @comment.save
+      redirect_to video_path(@video), notice: 'コメントが送信されました'
+    else
+      @comments = @video.comments.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id)
   end
 end
