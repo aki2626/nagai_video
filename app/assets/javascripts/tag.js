a41b8010a375cadd97b1967278fe40c2966d4c03
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-29 14:27:12 +0900

    addTagの定義

--- app/assets/javascripts/tag.js
+++ app/assets/javascripts/tag.js
@@ -1,13 +1,20 @@
 $(function () {
-  function addTag(tag) {
+  function indexTag(tag) {
     let html = `
     <div class="tags_lists__tag">
       ${tag.name}
-
     </div>
     `;
     $(".tags_js_index").append(html);
 
+  }
+  function addTag(tag) {
+    let html = `
+    <div class="tags_lists__tag">
+      ${tag}
+    </div>
+    `;
+    $(".tags_js_index").append(html);
   }
 
   $(".tags_area_regular__title").on("click", function(e) {
@@ -20,19 +27,18 @@ $(function () {
       dataType: "json"
     })
     .done(function(tags) {
-      console.log(tags)
       tags.forEach(function(tag) {
-        addTag(tag);
+        indexTag(tag);
       })
     })
     .fail(function(tags) {
       console.log("失敗です。");
-      console.log(tags);
     });
   });
   $(".tags_add_form__submit").on("click", function(e){
     e.preventDefault();
     let input = $(".tags_add_form__text").val();
     console.log(input);
+    addTag(input);
   });
 })
