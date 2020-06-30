8f855d55dee0d8aa0a9267a9a350ef46a7d4c369
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-30 21:16:03 +0900

    各モデルの更新
    user.rbとsns_credential.rbの更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   validates :nickname, presence: true
   has_one   :user_detail
   has_many  :videos
   has_many  :comments
   has_many  :mylists, dependent: :destroy
   has_many  :viewing_histories, dependent: :destroy 
+  has_many :sns_credentials
 end
