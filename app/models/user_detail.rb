56d61d703dee62a909f6baed0887079a8c7397de
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-07-02 21:05:11 +0900

    user_detail.rbの更新
    imageのアップロードをImageuploaderで行えるようにする。

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -2,7 +2,7 @@ class UserDetail < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
-  mount_uploader :image, MovieUploader
+  mount_uploader :image, ImageUploader
   
   belongs_to :user, optional: true
   validates :gender, :prefecture_id, :birth_date, presence: true
